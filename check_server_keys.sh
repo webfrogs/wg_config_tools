3a99e920f14794f20a4eecf4de75432c8fbfed39
Carl Chen <ccf.developer@gmail.com>
2020-01-03 17:59:43 +0800

    [WIP] server key script

--- check_server_keys.sh
+++ check_server_keys.sh
@@ -8,10 +8,26 @@ fi
 
 configFolderPath=/etc/wireguard
 
-if test -f "${configFolderPath}/server_keys/privatekey" -a -f "${configFolderPath}/server_keys/publickey"; then
-	echo "===> OK! Server keys exist."
-	exit
+echo "===> Checking basic settings"
+basicSettingFilePath=${configFolderPath}/basic_env.sh
+if test -f "${basicSettingFilePath}"; then
+	source "${basicSettingFilePath}"
 fi
 
-wg genkey | tee ${configFolderPath}/server_keys/privatekey | wg pubkey > ${configFolderPath}/server_keys/publickey
+if test -z "${WG_INTERFACE}"; then
+	WG_INTERFACE=wg0
+	echo "export WG_INTERFACE=${WG_INTERFACE}" >> ${configFolderPath}
+fi
+echo "- wireguard interface: ${WG_INTERFACE}"
+
+echo "===> Checking server keys..."
+if test -f "${configFolderPath}/server_keys/privatekey" -a -f "${configFolderPath}/server_keys/publickey"; then
+	echo "OK! Server keys exist."
+else
+	echo "Server keys are not found. Creating server keys..."
+	mkdir -p ${configFolderPath}/server_keys
+	wg genkey | tee ${configFolderPath}/server_keys/privatekey | wg pubkey > ${configFolderPath}/server_keys/publickey
+	echo "Done"
+fi
+
 
